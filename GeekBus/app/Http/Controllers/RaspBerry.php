02491cc6ea3db94b36d1ca9679f6acdd83264a93
aleorion <aletmdec@hotmail.com>
2016-09-28 09:23:40 -0500

    esqueleto de controladores

--- GeekBus/app/Http/Controllers/RaspBerry.php
+++ GeekBus/app/Http/Controllers/RaspBerry.php
@@ -10,16 +10,66 @@ class RaspBerry extends Controller
 {
     public function login(Request $request)
     {
-        return "login";
+        /*recibe 
+            ::chofer?%s
+            ::idRuta?%s
+        */
+        $chofer = $request->chofer;
+        $ruta = $request->idRuta;
+
+        /*repuesta 
+            ::success?%b
+            ::auth?%d
+        */
+        $success = true;
+        $auth = 123;
+
+        $assoc = array("success"=>$success,"auth"=>$auth);
+        $respuesta = json_encode($assoc);
+        return $respuesta;
     }
 
     public function update(Request $request)
     {
-        return "update";
+        /*recibe 
+            ::auth?%d
+            ::rpm?%d
+            ::pasajeros?%d
+            ::velocidad?%d
+            ::lat?%lf
+            ::long?%lf
+        */
+        $auth = $request->auth;
+        $rpm = $request->rpm;
+        $pasajeros = $request->pasajeros;
+        $velocidad = $request->velocidad;
+        $lat = $request->lat;
+        $long = $request->long;
+
+        /*respuesta 
+            ::success?%b
+        */
+        $success = true;
+
+        $assoc = array("success"=>$success);
+        $respuesta = json_encode($assoc);
+        return $respuesta;
     }
 
     public function shutdown(Request $request)
     {
-        return "shutdown";
+        /*recibe 
+            ::auth?%d
+        */
+        $auth = $request->auth;
+
+        /*respuesta 
+            ::success?%b
+        */
+        $success = true;
+
+        $assoc = array("success"=>$success);
+        $respuesta = json_encode($assoc);
+        return $respuesta;
     }
 }
