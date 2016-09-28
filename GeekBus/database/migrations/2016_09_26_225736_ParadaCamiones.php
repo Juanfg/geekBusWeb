71732339153b759cf852009052d990ee751ab192
Ricardo Lopez Focil <rikyfocil@Ricardos-MacBook-Pro.local>
2016-09-28 10:48:07 -0500

    Fixed database migrations

--- GeekBus/database/migrations/2016_09_26_225736_ParadaCamiones.php
+++ GeekBus/database/migrations/2016_09_26_225736_ParadaCamiones.php
@@ -14,11 +14,13 @@ class ParadaCamiones extends Migration
     public function up()
     {
         Schema::create('ParadaCamiones', function (Blueprint $table) {
+            $table->increments('idParadaCamion');
             $table->integer('idRuta')->unsigned();
             $table->foreign('idRuta')->references('idRuta')->on('Rutas')->onDelete('cascade');
             $table->integer('idParada')->unsigned();
             $table->foreign('idParada')->references('idParada')->on('Paradas')->onDelete('cascade');
             $table->integer('numParada');
+            $table->timestamps();
         });
     }
 
