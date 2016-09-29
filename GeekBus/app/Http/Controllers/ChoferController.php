034525767f145a6276e7cfcdf1aac12a3c6e1758
Ricardo Lopez Focil <rikyfocil@Ricardos-MacBook-Pro.local>
2016-09-29 16:58:33 -0500

    Ahora conductores permite editar

--- GeekBus/app/Http/Controllers/ChoferController.php
+++ GeekBus/app/Http/Controllers/ChoferController.php
@@ -37,7 +37,6 @@ class ChoferController extends Controller
      */
     public function store(Request $request)
     {
-        $message = "";
         do{
             $this->validate($request,[
                 "nombre" => "required|string",
@@ -45,7 +44,7 @@ class ChoferController extends Controller
                 "image" => "required|image",
             ]);
 
-            $path = $request->image->store('images');
+            $path = $request->image->store("");
 
             $alreadyExists = Conductor::where("loginKey",$request->loginKey)->count();
 
@@ -72,7 +71,8 @@ class ChoferController extends Controller
      */
     public function show($id)
     {
-
+        $conductor = Conductor::where('idConductor', $id)->firstOrFail();
+        return view('conductores.show', ['conductor' => $conductor]);
     }
 
     /**
@@ -83,7 +83,7 @@ class ChoferController extends Controller
      */
     public function edit($id)
     {
-        $conductor = Conductor::where('idConductor', 2)->firstOrFail();
+        $conductor = Conductor::where('idConductor', $id)->firstOrFail();
         return view('conductores.editar', ['conductor' => $conductor]);
     }
 
@@ -96,8 +96,37 @@ class ChoferController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
-    }
+        do{
+
+            $conductor = Conductor::where('idConductor' , $id)->firstOrFail();
+
+            $this->validate($request,[
+                "nombre" => "required|string",
+                "loginKey" => "required|string",
+                "image" => "image",
+            ]);
+
+            $updating = $request->all();
+
+            if ($request->hasFile('image')){
+                $updating['fotoPath'] = $request->image->store("");
+            }
+            
+            $alreadyExists = Conductor::where("loginKey",$request->loginKey)->where('idConductor', '<>' ,$id)->count();
+
+            if($alreadyExists == 0){
+                //no existe
+                $conductor->update($updating);
+            }else{
+                //existe
+                $request->session()->flash('error', "La llave ya esta en uso por otro usuario. Es muy importante que sea &uacute;nica");
+                return back()->withInput();
+            }
+
+        }while(false);
+
+        $request->session()->flash("message", "Actualizado con exito");
+        return redirect()->route("choferes.show", [$id]);    }
 
     /**
      * Remove the specified resource from storage.
